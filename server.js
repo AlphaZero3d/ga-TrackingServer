5836362d5bf0148070c7e4572c89b13ff605de4e
AlphaZero3d <111949729+AlphaZero3d@users.noreply.github.com>
2024-08-11 15:01:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 // Store tracked item IDs
 let trackedItems = [];
 
