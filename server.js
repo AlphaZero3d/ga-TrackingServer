0acbf16958f3dcda2c0b24e7c1a2c84d545f4935
AlphaZero3d <111949729+AlphaZero3d@users.noreply.github.com>
2024-08-11 14:45:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Store tracked item IDs
 let trackedItems = [];
